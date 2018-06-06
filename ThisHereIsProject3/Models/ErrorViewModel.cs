faf62e282577f4ee58d0616e820a738e67cdf324
A.C. Miller <acmiller03@gmail.com>
2018-06-06 13:01:14 -0700

    Add project files.

---
+++ ThisHereIsProject3/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace ThisHereIsProject3.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
